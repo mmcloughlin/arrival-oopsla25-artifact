8e39d8349bb222098a7a30a59aa3165c5bd67870
Michael McLoughlin <mcloughlin@cmu.edu>
2025-07-01 19:56:55 -0400

    build archive in output directory

--- script/archive.sh
+++ script/archive.sh
@@ -6,8 +6,16 @@ base="arrival-oopsla25-artifact"
 archive_file="${base}.zip"
 doc_file="${base}.pdf"
 
+# Setup output directory.
+output_dir="build"
+rm -rf "${output_dir}"
+mkdir -p "${output_dir}"
+
 # Render the documentation.
 ./script/render_doc.sh "${doc_file}"
 
 # Prepare the archive.
 git-archive-all --include="${doc_file}" "${archive_file}"
+
+# Move to output directory.
+mv "${doc_file}" "${archive_file}" "${output_dir}"
